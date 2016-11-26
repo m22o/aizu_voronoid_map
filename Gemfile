65186a7499b0e5bc9ff0c1194dccdc642723d901
takurou Matsuo <m.taku7@gmail.com>
2016-11-26 20:52:54 +0900

    とりあえず完成

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@
 source "https://rubygems.org"
 
 gem 'rubyvor', '~> 0.1.4'
+gem 'mongo'
+gem 'dotenv'
--- voronoi.rb
+++ voronoi.rb
@@ -1,16 +1,28 @@
 require 'ruby_vor'
 require 'pp'
 require 'pry'
+require 'json'
+json_file_path = 'data.json'
+MAX_NUM = 5000
 
-points = [
-  RubyVor::Point.new(120, 290),
-  RubyVor::Point.new(110, 120),
-  RubyVor::Point.new(160, 90.2),
-  RubyVor::Point.new(3.14159265, 3.14159265)
-]
+json_data = open(json_file_path) do |io|
+  JSON.load(io)
+end
 
+points = []
+json_data.each do |place|
+  points.push(RubyVor::Point.new(place["latitude"]*MAX_NUM, place["longitude"]*MAX_NUM))
+end
+
+
+# RubyVor::Visualizer.make_svg(comp, :name => 'tri.svg')
 
 # Compute the diagram & triangulation
+# binding.pry
 comp = RubyVor::VDDT::Computation.from_points(points)
+# pp comp.nn_graph
+# pp comp.minimum_spanning_tree
+
+RubyVor::Visualizer.make_svg(comp, :name => 'dia.svg', :voronoi_diagram => true)
 
 RubyVor::Visualizer.make_svg(comp, :name => 'tri.svg')
